8bced7154c26337afb6e6053fcc60a907dd8a9fa
Mr Rahul <ashish227087@gmail.com>
2025-04-06 11:35:05 +0530

    adding Forecast of weather

--- script.js
+++ script.js
@@ -8,141 +8,148 @@ const validate = () => {
     }
 
 }
+const GetWeather = async () => {
+    try {
+        let value = city.value;
+        let Url = `https://goweather.xyz/weather/${value}`;
+        let response = await fetch(Url);
 
-const GetWeather = () => {
-    let value = city.value;
-    let Url = `https://goweather.xyz/weather/${value}`
-    let p = fetch(Url);
-    p.then((response) => {
-        return response.json()
-    }).then((data) => {
+        if (!response.ok) {
+            throw new Error("Failed to fetch weather data.");
+        } else if (!response.status) {
+            throw new Error("Failed to fetch the weather statu")
+        };
+
+        let data = await response.json();
+
         temp.innerHTML = data.temperature;
-        temp1.innerHTML = data.temperature;
-        temp2.innerHTML = data.temperature;
-        temp3.innerHTML = data.temperature;
         wind.innerHTML = data.wind;
-        wind1.innerHTML = data.wind;
-        wind2.innerHTML = data.wind;
-        wind3.innerHTML = data.wind;
         desc.innerHTML = data.description;
-        desc1.innerHTML = data.description;
-        desc2.innerHTML = data.description;
-        desc3.innerHTML = data.description;
-        console.log(data);
-    })
-}
+
+        const day1 = data.forecast[0];
+        const day2 = data.forecast[1];
+        const day3 = data.forecast[2];
+
+        day1Temp.innerHTML = day1.temperature;
+        day2Temp.innerHTML = day2.temperature;
+        day3Temp.innerHTML = day3.temperature;
+
+        day1wind.innerHTML = day1.wind;
+        day2wind.innerHTML = day2.wind;
+        day3wind.innerHTML = day3.wind;
+
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+        alert("Something went wrong! Please check the city name or try again later.");
+    }
+};
+
 
 
 async function dubaiForecast() {
-    
+
     let apiUrl = "https://goweather.xyz/weather/dubai";
     let promise = fetch(apiUrl);
     promise.then((response) => {
         return response.json();
     }).then((data) => {
- console.log(data)
+
         const day1 = data.forecast[0];
-        day1_temp.innerHTML= day1.temperature;
+        day1_temp.innerHTML = day1.temperature;
         day1_wind.innerHTML = day1.wind;
-       
+
         const day2 = data.forecast[1];
         day2_temp.innerHTML = day2.temperature;
-        day2_wind.innerHTML= day2.wind;
-       
+        day2_wind.innerHTML = day2.wind;
+
         const day3 = data.forecast[2];
         day3_temp.innerHTML = day3.temperature;
-        day3_wind.innerHTML= day3.wind;
-        
+        day3_wind.innerHTML = day3.wind;
+
 
     });
 };
 
 async function americaForecast() {
-    
+
     let apiUrl = "https://goweather.xyz/weather/dubai";
     let promise = fetch(apiUrl);
     promise.then((response) => {
         return response.json();
     }).then((data) => {
- console.log(data)
+
         const day1 = data.forecast[0];
-        day1_temp.innerHTML= day1.temperature;
+        day1_temp.innerHTML = day1.temperature;
         day1_wind.innerHTML = day1.wind;
-       
+
         const day2 = data.forecast[1];
         day2_temp.innerHTML = day2.temperature;
-        day2_wind.innerHTML= day2.wind;
-       
+        day2_wind.innerHTML = day2.wind;
+
         const day3 = data.forecast[2];
         day3_temp.innerHTML = day3.temperature;
-        day3_wind.innerHTML= day3.wind;
-        
+        day3_wind.innerHTML = day3.wind;
+
 
     });
 };
 
 
 async function americaForecast() {
-    
+
     let apiUrl = "https://goweather.xyz/weather/america";
     let promise = fetch(apiUrl);
     promise.then((response) => {
         return response.json();
     }).then((data) => {
- console.log(data)
+
         const day1 = data.forecast[0];
-        aday1_temp.innerHTML= day1.temperature;
+        aday1_temp.innerHTML = day1.temperature;
         aday1_wind.innerHTML = day1.wind;
-       
+
         const day2 = data.forecast[1];
         aday2_temp.innerHTML = day2.temperature;
-        aday2_wind.innerHTML= day2.wind;
-       
+        aday2_wind.innerHTML = day2.wind;
+
         const day3 = data.forecast[2];
         aday3_temp.innerHTML = day3.temperature;
-        aday3_wind.innerHTML= day3.wind;
-        
+        aday3_wind.innerHTML = day3.wind;
+
 
     });
 };
 
 async function chinaForecast() {
-    
+
     let apiUrl = "https://goweather.xyz/weather/china";
     let promise = fetch(apiUrl);
     promise.then((response) => {
         return response.json();
     }).then((data) => {
- console.log(data)
+
         const day1 = data.forecast[0];
-        cday1_temp.innerHTML= day1.temperature;
+        cday1_temp.innerHTML = day1.temperature;
         cday1_wind.innerHTML = day1.wind;
-       
+
         const day2 = data.forecast[1];
         cday2_temp.innerHTML = day2.temperature;
-        cday2_wind.innerHTML= day2.wind;
-       
+        cday2_wind.innerHTML = day2.wind;
+
         const day3 = data.forecast[2];
         cday3_temp.innerHTML = day3.temperature;
-        cday3_wind.innerHTML= day3.wind;
-        
+        cday3_wind.innerHTML = day3.wind;
+
 
     });
 };
-
-
-
-
 
 submitbtn.addEventListener('click', (e) => {
     e.preventDefault();
     validate();
     GetWeather()
-    document.getElementById("weatherAbout").innerHTML = `${city.value.toUpperCase()}`
-})
-
-
-
+    let cityName = city.value;
+    document.getElementById("weatherAbout").innerHTML = `${cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase()}`
+});
 dubaiForecast();
 americaForecast();
 chinaForecast();
